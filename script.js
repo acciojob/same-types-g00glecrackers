197d3adcf9c55fe17cdd16a0a04ae9f1e9c4669f
Mashkur Ahmed <119758913+g00glecrackers@users.noreply.github.com>
2026-02-25 15:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function isSameType(value1, value2) {
   //your js code here
+	if (value1 = Number && value2 = Number){
+		return true;
+	} else if (value1 = string && value2 = string) {
+		return true;
+	} else if (value1 = undefined && value2 = undefined){
+		return true;
+	} else {
+		return false;
+	}
+	  
 }
-
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
-alert(isSameType(value1, value2));
+alert(isSameType(value1, value2)); 
